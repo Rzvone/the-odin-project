2bad018306483ae702be7e403042d839426233f1
Roggo <bobonea.razvan.ctin@gmail.com>
2023-02-12 10:56:37 +0200

    added fundamentals part 1

---
+++ 08. JavaScript Basics/1 fundamentals part 1/script.js
@@ -0,0 +1,35 @@
+// 1
+
+console.log(23 + 97);
+
+// 2
+
+let sum = 1 + 2 + 3 + 4 + 5 + 6;
+
+console.log(sum);
+
+// 3
+
+let solution = (4 + 6 + 9) / 77;
+
+console.log(solution);
+
+// 4
+
+let a = 10;
+
+console.log(a);
+
+console.log(9 * a);
+
+let b = 7 * a;
+
+console.log(b);
+
+const MAX = 57;
+
+let actual = MAX - 13;
+
+let percentage = actual / MAX;
+
+console.log(percentage);
